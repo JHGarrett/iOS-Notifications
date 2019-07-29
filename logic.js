842a8f9bb4ab6d3994afc470ac4d97646652cb12
John Garrett <infantrygarrett@icloud.com>
2019-07-28 20:44:33 -0500

    working on js logic

---
+++ logic.js
@@ -0,0 +1,27 @@
+const lis = Array.from(document.querySelectorAll('li'));
+const li = lis[0];
+
+function getOpacity(i) {
+    return (i * 10) / 100;
+}
+
+function getHeight(li) {
+    return li.getBoundingClientRect().height;
+}
+
+function getStackedTop(i) {
+    return `${3 * i}px`;
+}
+
+lis.forEach((li,i) => {
+    li.style['z-index'] = -i;
+    li.style.top = getStackedTop(i);
+    li.style.opacity = i > 0 ? getOpacity(i) : 1;
+});
+
+li.addEventListener('click', function() {
+    lis.forEach((li, i) => {
+        const isOpen = this.classList.contains('open');
+        
+    })
+})
